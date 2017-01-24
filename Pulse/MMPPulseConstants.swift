4df26d2d99f0b03e97b34edee01346f73d7b1b97
Midhun MP <midhunmp7@gmail.com>
2017-01-25 01:03:28 +0530

    Fixed the access control level issues and updated podspec

--- Pulse/MMPPulseConstants.swift
+++ Pulse/MMPPulseConstants.swift
@@ -14,7 +14,7 @@
 /// - radar: Single wave like animation, one at a time
 /// - continous: Pulse expands and shrinks continuously
 /// - multiRadar: Multiple waves continuously displayed
-enum MMPPulseType
+public enum MMPPulseType
 {
     case radar
     case continous
@@ -24,23 +24,23 @@ enum MMPPulseType
 // MARK:- Structs
 
 /// Defines the constants used in the library
-struct MMPPulseConstants
+public struct MMPPulseConstants
 {
     // MARK:- Layer names
-    struct Layers
+    public struct Layers
     {
         static let outerLayer = "MMPOuterLayer"
     }
     
     // MARK:- Animation keypaths
-    struct Animations
+    public struct Animations
     {
         static let scale   = "transform.scale.xy"
         static let opacity = "opacity"
     }
     
     // MARK: Animation keys
-    struct AnimKeys
+    public struct AnimKeys
     {
         static let pulseAnimation = "MMP_Pulse_Animation"
     }
